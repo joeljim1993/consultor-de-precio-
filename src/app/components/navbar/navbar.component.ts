5d8588b3ee1070ef9a30f2710c66609b9c07a035
joel jimenez <joel_jimenez@live.com>
2023-12-07 15:29:43 -0400

    cambios en tasa del dolar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit, Pipe } from '@angular/core';
-import { tap } from 'rxjs/operators';
-import { KanaService } from 'src/app/services/kana-service.service';
+import {  Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -11,28 +9,12 @@ import { KanaService } from 'src/app/services/kana-service.service';
   ],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  {
 
   public textNavbar:string= "Consultor de Precio "
-  public priceDollar:number=0;
 
-  constructor( private kanaService:KanaService){
-
-  }
-
-ngOnInit(): void {
-  this.kanaService.priceDivisa$
-    .pipe(
-      tap(info => console.log("lo que llega en navbar",info)),
-      tap(price => this.priceDollar = price),
-      tap(()=>console.log("este es el precio del dolar",this.priceDollar)
-       )
-    ).subscribe();
-
-    console.log("priceDollar despues del oninit",this.priceDollar)
-}
 
 
  }

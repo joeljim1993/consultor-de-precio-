54b022f0e0f9c901f16398c111144cacf1a7576d
joel jimenez <joel_jimenez@live.com>
2024-04-25 20:56:16 -0400

    cambios en estructura de kanaService

--- src/app/services/kana-service.service.ts
+++ src/app/services/kana-service.service.ts
@@ -3,47 +3,50 @@ import { of, Observable, BehaviorSubject, tap, mergeMap, map } from 'rxjs';
 import { fromFetch } from 'rxjs/fetch';
 import { Product } from '../interfaces/productForKana.interface';
 import { Data, Edge, CurrentPriceList } from '../interfaces/kana-service.interface';
-
-
-
+import { Response} from '../interfaces/dollar-value.interface'
 @Injectable({
   providedIn: 'root',
 })
 export class KanaService {
 
+// todo: hacer la interfaces con un archivo de barrido index.ts
+
+
   // arreglo que guarda los pdoructos buscados
   public lastSearchedProducts: Product[] = [];
 
   // lista de productos temporales
-  public listProducts: Product[] = [];
+  public productsList: Product[] = [];
 
-  // productos traidos de kana directamente
-  // Todo: colocar tipo de dato y se puede usar signal
-  public productsKana = new BehaviorSubject<any>('sin datos');
+  //TODO:si la lista es null, se deberia mostrar un loading
+  public listProductsOfKana    = new BehaviorSubject<Product[]|null>(null);
   // producto resultado de la busqueda
-  // Todo: colocar tipo de dato y se puede usar signal
-  public productFound$ = new BehaviorSubject<any>("0");
-
-  // Todo: colocar tipo de dato y se puede usar signal
-  public lastSearchedProducts$ = new BehaviorSubject<any>("sin productos");
-
-  public priceDivisa$= new BehaviorSubject<number>(0);
-
+  // TODO: colocar el tipo de datos
+  public productFound$         = new BehaviorSubject<any>("0");
+  public lastSearchedProducts$ = new BehaviorSubject<Product[]|null>(null);
+  //TODO: trabajar con una señal computada , para solo lectura
+  public priceDivisa$          = new BehaviorSubject<number>(0);
 
   constructor() {
 
+
     this.getListProductFromKana$()
+      .pipe(tap(() => console.log('products.....', this.productsList)))
       .subscribe();
+
+    this.listProductsOfKana.subscribe();
 
       this.getDolarValue$()
       .pipe(
-        tap( price => this.priceDivisa$.next(price) ), )
-      .subscribe();
+        tap( price => this.priceDivisa$.next(price) ),
+
+      )
+      .subscribe()
+
   }
 
   getQuery(query: string) {
     const url = 'https://kana.develop.cecosesola.imolko.net/graphql';
-
     const dataQuery = {
       operationName: null,
       variables: {},
@@ -57,17 +60,13 @@ export class KanaService {
       body: JSON.stringify(payload),
       headers: new Headers({ 'content-type': 'application/json' }),
     };
-    // TODO: SE PUDIERA MANEJAR CON HTTP-CLIENT
-    const data$ = fromFetch(url, option)
-      .pipe(
-        mergeMap((resp: any) => resp.json()));
+
+    const data$ = fromFetch(url, option).pipe(mergeMap((resp: any) => resp.json()));
 
     return data$;
   }
 
-  // todo: que pasa si el limit aumenta> limit
-
-  getListProductFromKana$(limit: number = 1000):Observable<void> {
+  getListProductFromKana$(limit: number = 1000):Observable<any> {
     const query = `
     query {
       currentPriceList{
@@ -100,39 +99,31 @@ export class KanaService {
         }
       }
     }`;
-    // todo: como se mejora la legibilidad del codigo
+
     const data$ = this.getQuery(query).pipe(
       tap((data:any) => console.log('data', data)),
       map(
-        ({
-          data: {
-            currentPriceList: {
-              products: { edges },
-            },
-          },
-        }) => {
+        ({data: {currentPriceList: { products: { edges }, },},}) => {
           edges.map((edge:Edge) => {
             let productsKana:Product = edge.node.product;
-            this.listProducts.push( productsKana );
-            this.productsKana.next( this.listProducts );
+            this.productsList.push( productsKana );
+            this.listProductsOfKana.next( this.productsList );
           });
         }
       )
     );
     return data$;
-
-
   }
 
   searchProduct(barcode: string): Product[] {
 
-    let foundProduct:Product[] = this.listProducts.filter(products => products.barcode === barcode);
+   let foundProduct:Product[] = this.productsList.filter(products => products.barcode === barcode);
+    // console.log("foundProduct",foundProduct);
     this.productFound$.next(foundProduct[0]);
     this.verifyLastSearched( foundProduct[0] );
 
     return foundProduct;
   }
-
 
   verifyLastSearched(searchedProduct:Product):void{
 
@@ -149,6 +140,7 @@ export class KanaService {
     }
     this.lastSearchedProducts.push( searchedProduct );
     this.lastSearchedProducts$.next( this.lastSearchedProducts  );
+    console.log(" this.lastSearchedProducts en el servicio",this.lastSearchedProducts);
 
   }
 
@@ -173,8 +165,7 @@ export class KanaService {
             },
           },
         } = response;
-        let priceDollar:number = +forSales[1].value;
-        return priceDollar;
+        return forSales[1].value;
       }),
 
     );
@@ -184,7 +175,6 @@ export class KanaService {
 
 
 }
-
 
 
 /*
